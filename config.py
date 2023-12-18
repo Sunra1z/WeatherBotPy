8a6df2896e30f7c80265c64c0c60e9945066589f
Kassymzhomart Tokayev <70940419+Sunra1z@users.noreply.github.com>
2023-12-19 01:06:08 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-TG_TOKEN = '6702281548:AAGl_P5I7OGAcEq6DX_b9NaZUAcDcNEt-p8' # Telegram bot token
-WEATHER_API_TOKEN = '18c8cd16292a685e089037a4be839bef' # OpenWeatherMap API token
+TG_TOKEN = 'enter_yours' # Telegram bot token
+WEATHER_API_TOKEN = 'enter_yours' # OpenWeatherMap API token
 DB_URL = 'sqlite+aiosqlite:///db.sqlite3' # Database URL
